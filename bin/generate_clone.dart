bb5caa147ce54134eac1b9b98db4a168aa6b5b03
Abdullah Zreqat <abdullah_zreqat@safeersoft.com>
2024-09-26 10:21:36 +0300

    Moved generate file to bin folder

--- bin/generate_clone.dart
+++ bin/generate_clone.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library;
 
-export 'src/generate_clone_base.dart';
+export '../lib/src/generate_clone_base.dart';
 
 // ignore_for_file: avoid_print, missing_whitespace_between_adjacent_strings
